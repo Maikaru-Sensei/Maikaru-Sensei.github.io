5a7fd67914edf221c026c6a8e9370b0c5f6b8fd7
Michael Zauner <mzauner98@gmail.com>
2024-09-05 10:25:45 +0200

    Add background animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:github_io/widgets/onboarding_slider.dart';
+import 'package:particles_flutter/component/particle/particle.dart';
+import 'package:particles_flutter/particles_engine.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,24 +42,63 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Center(
-          child: Card(
-              elevation: 12,
-              color: Colors.black,
-              child: Container(
-                width: 400,
-                height: 600,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.black, width: 5),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: OnboardingSlider(), // Place the onboarding slider here
-                ),
-              ))),
+      body: Stack(children: [
+        Container(
+          color: Color(0xFF424242),
+          child: Particles(
+            awayRadius: 100,
+            particles: _createParticles(),
+            height: screenHeight,
+            width: screenWidth,
+            onTapAnimation: true,
+            awayAnimationDuration: const Duration(milliseconds: 100),
+            awayAnimationCurve: Curves.linear,
+            enableHover: true,
+            hoverRadius: 90,
+            connectDots: true,
+          ),
+        ),
+        Center(
+            child: Card(
+                elevation: 12,
+                color: Colors.black,
+                child: Container(
+                  width: 400,
+                  height: 600,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(color: Colors.black, width: 5),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child:
+                        OnboardingSlider(), // Place the onboarding slider here
+                  ),
+                )))
+      ]),
     );
+  }
+
+  List<Particle> _createParticles() {
+    var rng = Random();
+    List<Particle> particles = [];
+    for (int i = 0; i < 140; i++) {
+      particles.add(Particle(
+        color: Colors.white.withOpacity(0.6),
+        size: rng.nextDouble() * 10,
+        velocity: Offset(rng.nextDouble() * 30 * _randomSign(),
+            rng.nextDouble() * 40 * _randomSign()),
+      ));
+    }
+    return particles;
+  }
+
+  double _randomSign() {
+    var rng = Random();
+    return rng.nextBool() ? 1 : -1;
   }
 }
