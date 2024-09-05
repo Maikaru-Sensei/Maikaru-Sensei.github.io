9f1ee4b1087f111645803d0b7a71ffdec8dda90b
Maikaru-Sensei <Maikaru-Sensei@users.noreply.github.com>
2024-09-05 08:27:09 +0000

    deploy: 5a7fd67914edf221c026c6a8e9370b0c5f6b8fd7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1721026332"
+    serviceWorkerVersion: "1169933889"
   }
 });
